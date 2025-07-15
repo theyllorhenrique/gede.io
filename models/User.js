8e4c19fcbe375d5955d0111599898503a11f9bf6
Theyllor <theyllorhenrique@gmail.com>
2025-07-15 18:13:59 -0300

    finalmente vai

--- models/User.js
+++ models/User.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
+ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
