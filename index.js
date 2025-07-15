c1f9141223b1e9704acd9733443d2e8a2302d917
Theyllor <theyllorhenrique@gmail.com>
2025-07-15 14:56:38 -0300

    esquesi de atualizar

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB conectado!');
+}).catch(err => {
+  console.error('Erro ao conectar:', err);
+});
 
 app.use(express.static('public'));
 
